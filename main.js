fb8b06cd852982d7048c2bb68a673286d46945dc
Yeeeeee1 <KostyaEK2006@gmail.com>
2024-06-24 00:19:15 +0300

    feat: add some animations

--- main.js
+++ main.js
@@ -2,6 +2,20 @@ const openBtn = document.getElementById("openBtn");
 const closeBtn = document.getElementById("closeBtn");
 const overlay = document.querySelector(".overlay");
 const modal = document.querySelector(".modal");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const signin = document.getElementById("signin");
+
+overlay.addEventListener("click", function () {
+  modal.style.animationName = "modalBack";
+  overlay.style.animationName = "overlayBack";
+});
+
+signin.addEventListener("click", function () {
+  console.log("email: " + email.value, "password: " + password.value);
+  modal.style.animationName = "modalBack";
+  overlay.style.animationName = "overlayBack";
+});
 
 openBtn.addEventListener("click", function () {
   modal.style.animationName = "modal";
